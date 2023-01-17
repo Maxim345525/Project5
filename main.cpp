96ff8eeac983a0307c1745cbe15c54bbd43d95e0
Maxim345525 <maximmaksimchyk@gmail.com>
2023-01-17 20:46:51 +0200

    add code

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+int main()
+{
+
+    system("chcp 1251 > null");
+    int x, y;
+    cout << "¬вед≥ть x: ";
+    cin >> x;
+    cout << "¬вед≥ть y: ";
+    cin >> y;
+   
+    for (size_t i = x; i <= y; i++)
+    {
+        if (i % 2 == 0)
+            cout << i << '\t';
+    }
+    cout << endl;
+    return 0;
+}
